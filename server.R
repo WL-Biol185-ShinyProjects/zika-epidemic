83465e6f08c2305c8c6f4fc817201e6b5a0839a6
Lil Kilgore <kilgorel19@mail.wlu.edu>
2017-11-07 11:51:48 -0500

    working on heat map

--- server.R
+++ server.R
@@ -1,11 +1,11 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
-
 Zika_Country_Data <- read_csv("~/zika-epidemic/Zika - Country Data.csv")
 Zika_State_Data<- read.csv("~/zika-epidemic/Zika - US State Data (2).csv")
 Zika_Country_Data$Date <- as.Date(Zika_Country_Data$Date, format = "%m/%d/%Y")
-
+geoJSON_map <- readRDS(file = "geoJSON_map.rds")
+geoJSON_map@data <- Zika_Country_Data
 
 function(input, output, session) {
 
@@ -18,6 +18,23 @@ function(input, output, session) {
       geom_point() +
       theme(axis.text.x = element_text(angle = 60, hjust = 1))
   })
+  
+  output$Map_Outbreak_Over_Time <- renderLeaflet({
+    
+    pal  <- colorBin("YlOrRd", geoJSON_map@data)
+    
+    leaflet(data = geoJSON_map) %>%
+      addTiles(options = tileOptions(noWrap = TRUE)) %>%
+      addPolygons(fillColor = ~pal(Confirmed)) %>%
+      
+      addLegend(pal = pal, Confirmed = ~density, opacity = 0.7, title = input$Confirmed,
+                position = "bottomright")
+    
+    
+  })
+  
+}
+
 
 
   output$Outbreak_By_State <- renderPlot({
@@ -30,5 +47,8 @@ function(input, output, session) {
     
   })
    
-
+  geoJSON_map <- readRDS(file = "geoJSON_map.rds")
+  geoJSON_map@data <- Zika_Country_Data
+  pal  <- colorBin("YlOrRd", geoJSON_map@data)
+  
 }
--- ui.R
+++ ui.R
@@ -1,32 +1,61 @@
 library(shiny)
 library(markdown)
+geoJSON_map <- readRDS(file = "geoJSON_map.rds")
 
-navbarPage("Navbar!",
+navbarPage("Zika Epidemic",
   
-  tabPanel(Background),
-  
+  tabPanel("Background"),
           
-  tabPanel("Zika Cases in Pan-America",
-      sidebarLayout(
-      sidebarPanel(
-      selectInput( inputId = 'Country',
+  navbarMenu("Zika Cases in Pan-America",
+              
+      tabPanel("Graph Over Time"),
+              sidebarLayout(
+                sidebarPanel(
+                selectInput( inputId = 'Country',
                    label   = 'Select a Country',
                    choices = unique(Zika_Country_Data$Country_Territory)
-                     )
-    ),
+                     )),
+              mainPanel(plotOutput("Outbreak_Over_Time"))
+              ),
+              
+      tabPanel("Heatmap Over Time"),
+              sliderInput(inputId = "Confirmed", 
+                          label= "Select a Date", 
+                          11/17/16, 
+                          10/05/17, 
+                          11/17/16, 
+                          step = NULL, 
+                          round = FALSE,
+                          format = NULL,
+                          locale = NULL, 
+                          ticks = TRUE, 
+                          animate = TRUE,
+                          width = NULL, 
+                          sep = ",",
+                          pre = NULL, 
+                          post = NULL, 
+                          timeFormat = NULL,
+                          timezone = NULL, 
+                          dragRange = TRUE,
+      
+    mainPanel(leafletOutput("Map_Outbreak_Over_Time"))
     
-    mainPanel(plotOutput("Outbreak_Over_Time"))
-    )
   ),
+  
+  
   
   tabPanel("Zika Cases in United States",
            selectInput(inputId = 'Region',
                        label= 'Select a Region',
+                       multiple= TRUE,
                        choices = unique(Zika_US_State_Data_2_$Region)
                        ),
  
    mainPanel(plotOutput("Outbreak_By_State"))
-   )
+   ),
+  tabPanel
+
+  )
 
 )
   
